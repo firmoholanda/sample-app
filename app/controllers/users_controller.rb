1ea43ce80e266290943b90ae762f9ea05d152a8b
firmo holanda <firmo@viize.com>
2020-02-10 17:19:51 -0300

    update chap 10.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,5 +39,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "please log in."
+        redirect_to login_url
+      end
+    end
   
 end
