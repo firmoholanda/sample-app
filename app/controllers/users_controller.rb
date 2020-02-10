10bf02e0e608e3ec2ab2e5b32700308ea582e070
firmo holanda <firmo@viize.com>
2020-02-10 17:13:40 -0300

    update chap 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,6 +22,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
