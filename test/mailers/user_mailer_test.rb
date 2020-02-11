c2d47d2b71ecdd93cad06a3a3586b7d39c889b9f
firmo holanda <firmo@viize.com>
2020-02-11 17:24:59 -0300

    create 12.3

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,7 +4,7 @@ class UserMailerTest < ActionMailer::TestCase
 
   test "account_activation" do
     user = users(:michael)
-    user.activation_token = User.new_token
+    user.reset_token = User.new_token
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
@@ -13,5 +13,5 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
-  
+
 end
